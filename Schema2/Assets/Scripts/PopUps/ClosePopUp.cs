534a70514cf5c368c9bfce453097beb1b931177b
dlbcol <57753443+dlbcol@users.noreply.github.com>
2025-10-08 11:58:53 +0200

    Comments bij UX scripts toegevoegd

--- Schema2/Assets/Scripts/PopUps/ClosePopUp.cs
+++ Schema2/Assets/Scripts/PopUps/ClosePopUp.cs
@@ -1,5 +1,10 @@
 using UnityEngine;
 
+/// <summary>
+/// This script is attatched to the pop up.
+/// It closes the pop up when the close button is clicked.
+/// </summary>
+
 public class ClosePopUp : MonoBehaviour
 {
     public void ClosePopUpButton()
