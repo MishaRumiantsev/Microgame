2bea2ba891ed9f1590d2daca2cf076b0136ee443
Gijs-Hoekstra <gijs1008@gmail.com>
2025-10-08 14:59:04 +0200

    updated script

--- Schema2/Assets/Scripts/Falling object.cs
+++ Schema2/Assets/Scripts/Falling object.cs
@@ -2,27 +2,22 @@ using UnityEngine;
 
 public class Fallingobject : MonoBehaviour
 {
-    public float fallSpeed = 1f;          public float zigZagSpeed = 2f;     
-    public float zigZagWidth = 2f;     
+    public float fallSpeed = 1f;
+    public float zigZagSpeed = 2f;
+    public float zigZagWidth = 2f;
     private float startX;
 
     void Start()
     {
-        // Save the starting X position
         startX = transform.position.x;
     }
 
     void Update()
     {
         float x = startX + Mathf.Sin(Time.time * zigZagSpeed) * zigZagWidth;
-
-        
         float y = transform.position.y - fallSpeed * Time.deltaTime;
-
-        
         transform.position = new Vector3(x, y, transform.position.z);
 
-        
         HandleInput();
     }
 
@@ -32,9 +27,7 @@ public class Fallingobject : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
-            {
                 CheckTouch(touch.position);
-            }
         }
         else if (Input.GetMouseButtonDown(0))
         {
@@ -44,16 +37,12 @@ public class Fallingobject : MonoBehaviour
 
     void CheckTouch(Vector2 screenPosition)
     {
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        RaycastHit hit;
+        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(screenPosition);
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-        if (Physics.Raycast(ray, out hit))
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
         {
-            Debug.Log("Hit: " + hit.collider.name);
-            if (hit.collider.gameObject == gameObject)
-            {
-                Destroy(gameObject);
-            }
+            Destroy(gameObject);
         }
     }
 }
