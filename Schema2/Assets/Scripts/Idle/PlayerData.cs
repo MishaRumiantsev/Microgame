ac88ee7c0cdc00b1cd1d4068782d6f38ebfb6089
Kiwibaas <120922887+Kiwibaas@users.noreply.github.com>
2025-10-10 15:16:28 +0200

    game saved now and filled in my log

---
+++ Schema2/Assets/Scripts/Idle/PlayerData.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+
+//how to call the stats
+//PlayerDataManager.Coins
+
+public class PlayerData
+{
+    public int coins;
+    public int storedSellPointCoins;
+    public int prestigeCoins;
+}
